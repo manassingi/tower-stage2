db06954331a3988053a74016176edd0d868600f9
manassingi <70809272+manassingi@users.noreply.github.com>
2020-10-16 22:11:18 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,46 @@
+class box{
+
+    constructor (x,y,width,height){
+    
+        var options={
+    
+            //isStatic:false,
+            restitution:0.3,
+			friction:0.5,
+            //density:1.2
+           
+        }
+        this.Visiblity = 255;
+        this.body=Bodies.rectangle(x,y, width, height, options);
+        this.width=width;
+        this.height=height;
+            World.add(world,this.body);
+    }
+    display(){
+        fill("yellow")
+        console.log(this.body.speed);
+        if(this.body.speed < 3.5){
+        var angle=this.body.angle;
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+        }
+        else{
+          World.remove(world, this.body);
+          push();
+          this.Visiblity = this.Visiblity - 8;
+          tint(255,this.Visiblity);
+          //image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+         pop();
+        }
+        
+      }
+     
+     
+    
+   
+}   
